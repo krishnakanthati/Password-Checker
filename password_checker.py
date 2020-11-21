8991579d07681235ff04dc3baa768f6bc6c35959
krishnakanthati <krishnakanthati@outlook.com>
2020-11-22 01:20:33 +0530

    password

--- password_checker.py
+++ password_checker.py
@@ -1,4 +1,4 @@
 username = input('Enter your username: ')
 password = input('Enter your password: ')
-password_length = len(password) * '*'
-print(f'{ username }, your passowrd is { password_length }.')
+hidden_password = len(password) * '*'
+print(f'{ username }, your passowrd is { hidden_password }.')
